fa919dd95a6774a04b5256fa840d89212a111d95
tramhuuducvn <tramhuuduc@gmail.com>
2023-05-10 21:30:09 +0700

    save!

--- MiniExercises/Main.java
+++ MiniExercises/Main.java
@@ -2,6 +2,6 @@ package MiniExercises;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(123);
     }
 }
---
+++ run.sh
@@ -0,0 +1,11 @@
+clear
+echo
+
+javac MiniExercises/Main.java
+java MiniExercises/Main
+
+# rm *.class
+# find . -name "*.class" -type f
+
+find . -name "*.class" -type f -delete
+echo
